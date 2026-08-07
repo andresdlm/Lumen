6535d7c188ef6910298aa086b22a5914d5d9e8dc
Andres Linarez <andreslinarez9@gmail.com>
2026-08-07 19:14:52 -0400

    Initial Commit

---
+++ Lumen/LumenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumenApp.swift
+//  Lumen
+//
+//  Created by Andres Linarez on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct LumenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
